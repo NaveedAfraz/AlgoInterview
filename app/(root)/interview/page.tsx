7865bc05c54aaec4effff8fa800a20d731d65550
NaveedAfraz <naveedafraz123@gmail.com>
2025-06-05 21:03:24 +0530

    added interview page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Agent from "@/components/agent";
+
+function Interview() {
+  return (
+    <>
+      <div>Interview</div>
+      <Agent userName="You" userId="user1" type="interview" />
+    </>
+  );
+}
+
+export default Interview;
---
+++ components/agent.tsx
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface AgentProps {
+  userName: string;
+  userId: string;
+  type: "generate" | "interview";
+}
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  ACTIVE = "ACTIVE",
+  CONNECTING = "CONNECTING",
+  FINISHED = "FINISHED",
+}
+
+function Agent({ userName, userId, type }: AgentProps) {
+  const isSpeaking = true;
+  const messages = ["Whats Your name?", "My name is naveen"];
+  const lastMessage = messages[messages.length - 1];
+  const callStatus = CallStatus.ACTIVE;
+console.log(lastMessage);
+  //   const handleCall = () => {
+  //     console.log("Call");
+  //   };
+
+  //   const handleDisconnect = () => {
+  //     console.log("Disconnect");
+  //   };
+
+  return (
+    <>
+      <div className="call-view">
+        {/* AI Interviewer Card */}
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interviewer</h3>
+        </div>
+
+        {/* User Profile Card */}
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect">End</button>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Agent;
